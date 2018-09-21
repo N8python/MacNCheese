1b20b2b8653e16b72b855578c0d5389965185f6b
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-20 22:19:53 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+e = 100*100*10+5*1
+for i = 1, e do 
+	print("Mac and cheese is the best!") 
+	end
+print("You can run and see the code here: https://repl.it/@jumbocakeyumyum/LuaNCheese")
+print("Thanks for running, the phrase just got printed",e,"times!")
