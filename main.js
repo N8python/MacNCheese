e3b375615ff545125b6341f9894da0ed0e7d06c5
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-20 22:45:52 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var e = 200;
+var iDone = false;
+for(var i = 0; i < e; i++) {
+  console.log("Mac and cheese is the best!")
+}
+if(i = e) {
+  iDone = true;
+}
+if(iDone) {
+  alert("Done!  Open up your console to see.")
+}
