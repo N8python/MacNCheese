563ee128cf84d9c3a241412491721ae1cb61b866
RDIL <34555510+RDIL@users.noreply.github.com>
2018-10-08 14:41:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 var e = 20;
-var iDone = false;
 for(var i = 0; i < e; i++) {
   console.log("Mac and cheese is the best!")
 }
-if(i = e) {
-  iDone = true;
-}
-if(iDone) {
-  alert("Open up your console")
-}
+alert("Open up your console")
